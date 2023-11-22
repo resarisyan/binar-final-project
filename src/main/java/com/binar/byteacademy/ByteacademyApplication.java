5a00dee62e90fb33e206e02c363a58b154991cff
resarisyan <resarisyan77@gmail.com>
2023-11-22 10:14:31 +0700

    initial commit

---
+++ src/main/java/com/binar/byteacademy/ByteacademyApplication.java
@@ -0,0 +1,13 @@
+package com.binar.byteacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteacademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteacademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binar/byteacademy/ByteacademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binar.byteacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteacademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
